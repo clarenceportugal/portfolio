ac6dfcc8163fccfeb09b34b8d2a8b8b55f2214b8
clarenceportugal <clarence.portugal12@gmail.com>
2026-01-23 10:21:23 +0800

    Update email address to clarence.portugal12@gmail.com

--- src/App.tsx
+++ src/App.tsx
@@ -1287,12 +1287,12 @@ function App() {
               to be part of your visions.
             </p>
             <div className="contact-info">
-              <a href="mailto:portugalclarence15@gmail.com?subject=Contact%20from%20Clarence%20Portugal%20Portfolio" className="contact-item">
+              <a href="mailto:clarence.portugal12@gmail.com?subject=Contact%20from%20Clarence%20Portugal%20Portfolio" className="contact-item">
                 <svg className="contact-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2-2-2z"></path>
                   <polyline points="22,6 12,13 2,6"></polyline>
                 </svg>
-                <span>portugalclarence15@gmail.com</span>
+                <span>clarence.portugal12@gmail.com</span>
               </a>
               <a href="https://www.facebook.com/renz42gal" target="_blank" rel="noopener noreferrer" className="contact-item">
                 <svg className="contact-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
